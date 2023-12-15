5aed1b1e64782ce1d76b1338da073c020d9387d2
calimadevteam <orlichtamara@gmail.com>
2023-12-15 08:59:13 +0000

    created folders

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { MoviesEffect } from './store/movies/movie.effect';
+import { movieReducer } from './store/movies/movie.reducer';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot(
+      {
+        movies: movieReducer
+      },
+      {}
+    ),
+    EffectsModule.forRoot([MoviesEffect]),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+
+}
+
