bf4cb0ce2d5085c12097222beee23a6cf50165e4
calimadevteam <orlichtamara@gmail.com>
2023-12-15 16:26:13 +0000

    home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesResponseData } from '../../models/movie';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { getPopularMovies } from '../../store/movies/movies.action';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  ngOnInit(): void {
+  movies$: Observable<MoviesResponseData>;
+  page: number = 1;
 
+  constructor(
+    private router: Router,
+    private store: Store) {
+    //@ts-ignore
+    this.movies$ = store.select('popularMovies');
+  }
+
+  ngOnInit(): void {
+    this.page = 1;
+    this.store.dispatch(getPopularMovies({ page: this.page }))
+  }
+
+  seeMoreMovies() {
+    this.page = this.page + 1;
+    this.store.dispatch(getPopularMovies({ page: this.page }))
   }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderModule } from '../../components/header/header.module';
+import { CardModule } from '../../components/card/card.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
+    HeaderModule,
+    CardModule,
     HomeRoutingModule
   ]
 })
