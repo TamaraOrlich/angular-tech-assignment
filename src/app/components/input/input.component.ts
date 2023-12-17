386de344e24e97b46422805c7e62d0fb7977f5bb
calimadevteam <orlichtamara@gmail.com>
2023-12-17 09:41:26 +0000

    Input component

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss'
+})
+export class InputComponent {
+  @Input() type: 'email' | 'password' | 'text' = 'text'
+  @Input() label!: string;
+  @Input() text: string = ''
+
+  @Output() textValue = new EventEmitter<string>();
+
+  emitValue() {
+    this.textValue.emit(this.text);
+  }
+
+}
